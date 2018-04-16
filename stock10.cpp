e873cd5016f73f3ded7c0d02e0b11403a9ed8b96
marklance <2927295165@qq.com>
2018-04-16 23:52:39 +0800

    10章例题

--- stock10.cpp
+++ stock10.cpp
@@ -5,11 +5,11 @@ Stock::Stock()
 	std::cout << "Default constructor called\n";
 	company = "no name";
 	shares = 0;
-	shares_val = 0.0;
+	share_val = 0.0;
 	total_val = 0.0;
 }
 
-void Stock::Stock(const std::string &co, long n, double pr)
+Stock::Stock(const std::string &co, long n, double pr)
 {
 	std::cout << "Constructor using " << co << " called\n";
 	company = co;
